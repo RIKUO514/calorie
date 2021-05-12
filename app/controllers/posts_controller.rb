91888c7128334046f1a43f08abcd6b6eb75e1120
RIKUO514 <r19960514@gmail.com>
2021-05-12 11:10:53 +0900

    投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,35 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(posts_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(posts_params)
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:name, :image, :text)
+  end
+
 end
